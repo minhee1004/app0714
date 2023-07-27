9bceccc6ffef55046188af9321cbc92ad1ae20ac
minhee1004 <alsgml5851@gmail.com>
2023-07-27 17:37:55 +0900

    mysql연동

---
+++ src/org/sp/app0714/table/TableMain.java
@@ -0,0 +1,49 @@
+package org.sp.app0714.table;
+//자바의 GUI 컴포넌트 중, 표형태의 레코드를 출력하기에
+//가장 최적화된 컴포넌트인 JTable을 이용해보자
+
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+public class TableMain extends JFrame{
+	JTable table;
+	JScrollPane scroll;
+	String[][] data= {
+			{"1","batman","배트맨","010"},
+			{"2","xman","엑스맨","016"},
+			{"3","spiderman","맨맨맨","011"}
+						
+	};
+	String[] column= {"member_idx","id","name","phone"};
+	
+	public TableMain() {
+		//table= new JTable(이차원배열의데이터,일차원배열);
+		table= new JTable(data,column);
+		scroll = new JScrollPane(table);
+
+		add(scroll);
+		
+		setVisible(true);
+		setSize(400,300);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	public static void main(String[] args) {
+		new TableMain();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
